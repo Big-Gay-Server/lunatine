f6bfc443b2687a2f63f60c3f1c437c88e12a6807
Lunar <lunarconstruct@gmail.com>
2026-03-27 16:10:56 -0400

    um??

--- parsedownGloss.php
+++ parsedownGloss.php
@@ -1,23 +1,33 @@
 <?php
-require_once 'Parsedown.php'; // Path to the original library
+require_once 'Parsedown.php'; 
 
 class ParsedownGloss extends Parsedown {
     protected function blockFencedCodeComplete($Block) {
+        // Only trigger for ```gloss blocks
         if (isset($Block['element']['attributes']['class']) && 
             $Block['element']['attributes']['class'] === 'language-gloss') {
             
-            $lines = explode("\n", $Block['element']['text']);
+            $text = $Block['element']['text'] ?? '';
+            $lines = explode("\n", $text);
             $newHtml = '';
 
             foreach ($lines as $line) {
+                // Fix: Skip empty lines to prevent "Deprecated" warnings
+                if (trim($line) === '') {
+                    $newHtml .= "\n";
+                    continue;
+                }
+
                 if (preg_match('/^(\\\\(\w+))\s*(.*)/', $line, $matches)) {
-                    $idName = $matches[2];
-                    $content = $matches[3];
+                    $fullId  = $matches[1]; // e.g. \ex
+                    $idName  = $matches[2]; // e.g. ex
+                    $content = $matches[3]; // The text
+                    
                     $newHtml .= "<div class=\"gloss-line gloss-$idName\">" .
-                                "<span class=\"gloss-id\">{$matches[1]}</span> " .
+                                "<span class=\"gloss-id\">$fullId</span> " .
                                 "<span>$content</span></div>\n";
                 } else {
-                    $newHtml .= "<div>$line</div>\n";
+                    $newHtml .= "<div>" . htmlspecialchars($line) . "</div>\n";
                 }
             }
 
@@ -27,5 +37,3 @@ class ParsedownGloss extends Parsedown {
         return $Block;
     }
 }
-
-// weh
