be331b97a35830e48063c74546c5a9f43e1fa342
Lunar <lunarconstruct@gmail.com>
2026-03-27 16:19:00 -0400

    graahhh

--- parsedownGloss.php
+++ parsedownGloss.php
@@ -1,39 +1,52 @@
 <?php
-require_once 'Parsedown.php'; 
+require_once 'Parsedown.php';
 
 class ParsedownGloss extends Parsedown {
-    protected function blockFencedCodeComplete($Block) {
-        // Only trigger for ```gloss blocks
-        if (isset($Block['element']['attributes']['class']) && 
-            $Block['element']['attributes']['class'] === 'language-gloss') {
-            
-            $text = $Block['element']['text'] ?? '';
-            $lines = explode("\n", $text);
-            $newHtml = '';
+    function __construct() {
+        // We add "gloss" to the list of fenced code blocks it looks for
+        $this->BlockTypes['`'][] = 'Gloss';
+        $this->BlockTypes['~'][] = 'Gloss';
+    }
 
-            foreach ($lines as $line) {
-                // Fix: Skip empty lines to prevent "Deprecated" warnings
-                if (trim($line) === '') {
-                    $newHtml .= "\n";
-                    continue;
-                }
-
-                if (preg_match('/^(\\\\(\w+))\s*(.*)/', $line, $matches)) {
-                    $fullId  = $matches[1]; // e.g. \ex
-                    $idName  = $matches[2]; // e.g. ex
-                    $content = $matches[3]; // The text
-                    
-                    $newHtml .= "<div class=\"gloss-line gloss-$idName\">" .
-                                "<span class=\"gloss-id\">$fullId</span> " .
-                                "<span>$content</span></div>\n";
-                } else {
-                    $newHtml .= "<div>" . htmlspecialchars($line) . "</div>\n";
-                }
-            }
-
-            $Block['element']['rawHtml'] = $newHtml;
-            unset($Block['element']['text']);
+    protected function blockGloss($Line) {
+        if (preg_match('/^([`]{3,}|[~]{3,})gloss/', $Line['text'])) {
+            $Block = [
+                'char' => $Line['text'][0],
+                'element' => [
+                    'name' => 'div',
+                    'attributes' => ['class' => 'gloss-container'],
+                    'handler' => 'elements',
+                ],
+            ];
+            return $Block;
         }
+    }
+
+    protected function blockGlossContinue($Line, $Block) {
+        if (isset($Block['complete'])) return;
+        if (isset($Line['text'][0]) && $Line['text'][0] === $Block['char'] && preg_match('/^'.$Block['char'].'{3,}(?:\s*$)/', $Line['text'])) {
+            $Block['complete'] = true;
+            return $Block;
+        }
+        
+        $text = $Line['text'];
+        if (preg_match('/^(\\\\(\w+))\s*(.*)/', $text, $matches)) {
+            $Block['element']['text'][] = [
+                'name' => 'div',
+                'attributes' => ['class' => "gloss-line gloss-{$matches[2]}"],
+                'handler' => 'elements',
+                'text' => [
+                    ['name' => 'span', 'attributes' => ['class' => 'gloss-id'], 'text' => $matches[1]],
+                    ['name' => 'span', 'attributes' => ['class' => 'gloss-content'], 'text' => $matches[3]],
+                ],
+            ];
+        } else {
+            $Block['element']['text'][] = ['name' => 'div', 'text' => $text];
+        }
+        return $Block;
+    }
+
+    protected function blockGlossComplete($Block) {
         return $Block;
     }
 }
