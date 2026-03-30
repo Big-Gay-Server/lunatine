d644f173fcc237a12468dbfb409b3fc573d50c58
Lunar <lunarconstruct@gmail.com>
2026-03-30 11:40:15 -0400

    boop

--- codetop.php
+++ codetop.php
@@ -40,15 +40,10 @@ $breadcrumbs = implode(' > ', $breadcrumbLinks);
         <center>
             <div class="logo"><a href="/">lunatine</a></div>
             <div class="nav">
-                <a href="/characters">characters</a>
-                <a href="/locations">locations</a>
-                <a href="/species">species</a>
-                <a href="/languages">languages</a>
-                <a href="/concepts">concepts</a>
-                <a href="/events">events</a>
-                <a href="/artifacts">artifacts</a>
-                <a href="/organizations">organizations</a>
-                <a href="/story">story</a>
+                <a href="/about">about</a>
+                <a href="/news">news</a>
+                <a href="/compendium">compendium</a>
+                <a href="/works">works</a>
             </div>
         </center>
     </div>
--- sidebar.php
+++ sidebar.php
@@ -1,18 +1,18 @@
 <div class="navigation">
-    <a href="/">home page</a>
     <div class="searchbar">
         search placeholder
     </div>
-    <div class="nav">
-                <h3>Navigation</h3>
-                <a href="/characters">characters</a></br>
-                <a href="/locations">locations</a></br>
-                <a href="/species">species</a></br>
-                <a href="/languages">languages</a></br>
-                <a href="/concepts">concepts</a></br>
-                <a href="/events">events</a></br>
-                <a href="/artifacts">artifacts</a></br>
-                <a href="/organizations">organizations</a></br>
-                <a href="/story">story</a></br>
-            </div>
+    <div class="quicklinks">
+        <h4>Compendium</h4>
+        <a href="/">home page</a>
+        <a href="/characters">characters</a></br>
+        <a href="/locations">locations</a></br>
+        <a href="/species">species</a></br>
+        <a href="/languages">languages</a></br>
+        <a href="/concepts">concepts</a></br>
+        <a href="/events">events</a></br>
+        <a href="/artifacts">artifacts</a></br>
+        <a href="/organizations">organizations</a></br>
+        <a href="/story">story</a></br>
+    </div>
 </div>
